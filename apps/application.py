1bea605d92ec7729b554a7d27af2be0dc29baa9b
Tony Thomas <thoma127@umn.edu>
2012-12-24 07:44:46 -0600

    More css/sass setup.

--- apps/application.py
+++ apps/application.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-	""Do stuff
+@app.route('/section/<section>')
+def index(section=None):
+	return render_template('index.html', section=section)
 
 if __name__ == '__main__':
-	app.run()
+	app.run(debug=True)
